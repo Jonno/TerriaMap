e615f9ae2ba7563c5acd921a94fc689f929c6092
Black Swan System <blackswansystem@gmail.com>
2020-08-11 14:26:43 +0930

    NLC Menu Item

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -34,7 +34,7 @@ export default function UserInterface(props) {
     <StandardUserInterface {...props} version={version}>
       <Menu>
         <RelatedMaps viewState={props.viewState} />
-        <MenuItem caption="About" href="about.html" key="about-link" />
+        <MenuItem caption="NLC" href="https://nlc.org.au" key="about-link" />
         <MenuItem
           caption="Permits"
           href="https://permits.nlc.org.au"
