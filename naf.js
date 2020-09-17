b0ade33a8bd5fdc73790c2aa4b7a51a8c211ada8
Black Swan System <blackswansystem@gmail.com>
2020-09-17 14:01:19 +0930

    Demo Bootstrap

--- naf.js
+++ naf.js
@@ -1,30 +1,27 @@
 <script>
-  (function detectIE() {
-  var ua = window.navigator.userAgent;
-  var msie = ua.indexOf('MSIE ');
-  var trident = ua.indexOf('Trident/');
-  var edge = ua.indexOf('Edge/');
-  if (msie > 0) {
-    // IE 10 or older
-    return true;
+  function detectIE(){
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+    var edge = ua.indexOf('Edge/');
+
+    if (msie > 0){
+      // IE 10 or older
+      return true;
+    }
+    else if (trident > 0){
+      return true;
+    }
+    else if (edge > 0){
+      return false;
+    }
+    else {
+      return false;
+    }
   }
-  else if (trident > 0) {
-    // IE 11
-    return true;
-  }
-  else if (edge > 0) {
-    // Edge
-    return false;
-  }
-  else
-    // other browser
-    return false;
-}
 
 if(!detectIE()) {
   alert('IE Detected');
 };
-
-)();
 
 </script>
